3a9fd0b1f7b59a27e1cc4ba7c0ec0cb1a4185a6c
Janggyoo Seo <jooyg62@naver.com>
2017-04-18 19:29:29 +0900

    commit 2

---
+++ InvalidArgumentException.java
@@ -0,0 +1,7 @@
+/**
+ * InvalidArgumentException Exception class
+ */
+public class InvalidArgumentException extends Exception {
+	public static final String ERROR_CONTENT = "This is Invalid Argument";
+
+}
